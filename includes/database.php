1f82835ead78afea0cc156c80584bef8267dde8e
mprz <marek.przepiorka@gmx.com>
2013-05-10 23:19:23 +0100

    updated installer

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,7 @@
 <?php
 // only change the 4 lines below
-$DB_SERVER  = 'localhost';
-$DB_USER    = 'root';
-$DB_PASS    = 'mysql';
-$DB_NAME    = "dom";
+$DB_SERVER  = '';
+$DB_USER    = '';
+$DB_PASS    = '';
+$DB_NAME    = "";
 ?>
--- install.php
+++ install.php
@@ -1,5 +1,18 @@
-<?php require_once ("includes/database.php"); ?>
 <?php
+
+require_once 'includes/database.php';
+
+$connection = mysql_connect($DB_SERVER, $DB_USER, $DB_PASS);
+    if (!$connection) {
+        echo 'Connection error';
+        die(mysql_error());
+    }
+
+$db_select = mysql_select_db($DB_NAME, $connection);
+if (!$db_select) {
+    echo 'Database select error';
+    die(mysql_error());
+}
 
 $query1 = "CREATE TABLE IF NOT EXISTS `domains` (
   `DomainID` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -10,6 +23,10 @@ $query1 = "CREATE TABLE IF NOT EXISTS `domains` (
   PRIMARY KEY (`DomainID`)
 );";
 $result1 = mysql_query($query1);
+if (!$result1) {
+    echo 'Table creation error (1)';
+    die(mysql_error());
+}
 
 $query2 = "CREATE TABLE IF NOT EXISTS `registrars` (
   `RegID` int(1) NOT NULL AUTO_INCREMENT,
@@ -19,7 +36,17 @@ $query2 = "CREATE TABLE IF NOT EXISTS `registrars` (
   PRIMARY KEY (`RegID`)
 );";
 $result2 = mysql_query($query2);
+if (!$result2) {
+    echo 'Table creation error (2)';
+    die(mysql_error());
+}
 
+$query3 = "INSERT INTO `registrars` (`RegName`, `RegLink`, `RegComment`) VALUES ('NameCheap', 'namecheap.com', 'sample registrar');";
+$result3 = mysql_query($query3);
+if (!$result3) {
+    echo 'Sample registrar not inserted!';
+    die(mysql_error());
+}       
 echo 'Tables created.';
 
 ?>
